34c0f68ea81f4dfc16893512fadfdf5bb1c6c2a3
ugorphpgo <ugorphpgo@gmail.com>
2026-09-24 18:55:11 +0300

    feat: render catalog cards from data in js file

--- assets/data/products.js
+++ assets/data/products.js
@@ -1,6 +1,6 @@
 const products = [
   {
-    id: 'coffe-1',
+    id: 'coffee-1',
     name: 'Irish coffee',
     image: 'assets/images/coffee-1-preview.webp',
     description: 'Fragrant black coffee with Jameson Irish whiskey and whipped milk',
@@ -37,7 +37,7 @@ const products = [
   },
 
   {
-    id: 'coffe-2',
+    id: 'coffee-2',
     name: 'Kahlua coffee',
     image: 'assets/images/coffee-2-preview.webp',
     description: 'Classic coffee with milk and Kahlua liqueur under a cap of frothed milk',
@@ -74,7 +74,7 @@ const products = [
   },
 
   {
-    id: 'coffe-3',
+    id: 'coffee-3',
     name: 'Honey raf',
     image: 'assets/images/coffee-3-preview.webp',
     description: 'Espresso with frothed milk, cream and aromatic honey',
@@ -111,7 +111,7 @@ const products = [
   },
 
   {
-    id: 'coffe-4',
+    id: 'coffee-4',
     name: 'Ice cappuccino',
     image: 'assets/images/coffee-4-preview.webp',
     description: 'Cappuccino with soft thick foam in summer version with ice',
@@ -148,7 +148,7 @@ const products = [
   },
 
   {
-    id: 'coffe-5',
+    id: 'coffee-5',
     name: 'Espresso',
     image: 'assets/images/coffee-5-preview.webp',
     description: 'Classic black coffee',
@@ -185,7 +185,7 @@ const products = [
   },
 
   {
-    id: 'coffe-6',
+    id: 'coffee-6',
     name: 'Latte',
     image: 'assets/images/coffee-6-preview.webp',
     description: 'Espresso coffee with the addition of steamed milk and dense milk foam',
@@ -222,7 +222,7 @@ const products = [
   },
 
   {
-    id: 'coffe-7',
+    id: 'coffee-7',
     name: 'Latte macchiato',
     image: 'assets/images/coffee-7-preview.webp',
     description: 'Espresso with frothed milk and chocolate',
@@ -259,7 +259,7 @@ const products = [
   },
 
   {
-    id: 'coffe-8',
+    id: 'coffee-8',
     name: 'Coffee with cognac',
     image: 'assets/images/coffee-8-preview.webp',
     description: 'Fragrant black coffee with cognac and whipped cream',
@@ -446,6 +446,7 @@ const products = [
 
   {
     id: 'dessert-1',
+    image: 'assets/images/dessert-1.webp',
     name: 'Marble cheesecake',
     description: 'Philadelphia cheese with lemon zest on a light sponge cake and red currant jam',
     price: '3.50',
@@ -482,6 +483,7 @@ const products = [
 
   {
     id: 'dessert-2',
+    image: 'assets/images/dessert-2.webp',
     name: 'Red velvet',
     description: 'Layer cake with cream cheese frosting',
     price: '4.00',
@@ -518,6 +520,7 @@ const products = [
 
   {
     id: 'dessert-3',
+    image: 'assets/images/dessert-3.webp',
     name: 'Cheesecakes',
     description:
       'Soft cottage cheese pancakes with sour cream and fresh berries and sprinkled with powdered sugar',
@@ -555,6 +558,7 @@ const products = [
 
   {
     id: 'dessert-4',
+    image: 'assets/images/dessert-4.webp',
     name: 'Creme brulee',
     description: 'Delicate creamy dessert in a caramel basket with wild berries',
     price: '4.00',
@@ -591,6 +595,7 @@ const products = [
 
   {
     id: 'dessert-5',
+    image: 'assets/images/dessert-5.webp',
     name: 'Pancakes',
     description: 'Tender pancakes with strawberry jam and fresh strawberries',
     price: '4.50',
@@ -627,6 +632,7 @@ const products = [
 
   {
     id: 'dessert-6',
+    image: 'assets/images/dessert-6.webp',
     name: 'Honey cake',
     description: 'Classic honey cake with delicate custard',
     price: '4.50',
@@ -663,6 +669,7 @@ const products = [
 
   {
     id: 'dessert-7',
+    image: 'assets/images/dessert-7.webp',
     name: 'Chocolate cake',
     description: 'Cake with hot chocolate filling and nuts with dried apricots',
     price: '5.50',
@@ -699,6 +706,7 @@ const products = [
 
   {
     id: 'dessert-8',
+    image: 'assets/images/dessert-8.webp',
     name: 'Black forest',
     description: 'A combination of thin sponge cake with cherry jam and light chocolate mousse',
     price: '6.50',
--- assets/scripts/catalog.js
+++ assets/scripts/catalog.js
@@ -1,19 +1,29 @@
 const grid= document.querySelector('.catalog__grid');
 
 function createCard (product){
-  const card = document.createElement('div')
-  card.className = 'card';
+  const card = document.createElement('div');
+  card.className = 'preview__card';
   card.dataset.id = product.id;
   card.innerHTML = `
   <div class="card__box">
-    <img class="card__image" src=${product.image} alt="Irish coffee">
+    <img class="card__image" src="${product.image}" alt="${product.name}">
       </div>
       <div class="preview__description">
         <div class="preview__title">
         <h2 class="preview__h2">${product.name}</h2>
         <p>${product.description}</p>
-        <p class="preview__price">${product.price}</p>
+        <p class="preview__price">$${product.price}</p>
         </div>
   </div>
   `;
+  return card;
 }
+
+function renderCatalog (category){
+  grid.replaceChildren();
+  products
+    .filter(product => product.category === category)
+    .forEach(product =>grid.append(createCard(product)));
+}
+
+renderCatalog('coffee');
